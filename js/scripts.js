fa0f41f00c69b15e577264d68b75db22452562df
Matthew Craig <mgcraig78@gmail.com>
2020-06-19 15:52:53 -0700

    refactor to test the total price function

--- js/scripts.js
+++ js/scripts.js
@@ -7,34 +7,35 @@ this.price = price;
 
 //basic price based on size
 Pizza.prototype.sizePrice = function(){
-  let sizeprice = 11;
-  if (this.size === "large"){
-    return sizeprice + 3;
+  let sizeprice = 0;
+  if (this.size === "small"){
+    sizeprice = 11;
   } else if (this.size === "medium") {
-    return sizeprice + 1;
-  } else {
-    return sizeprice;
-}
+    sizeprice = 12;
+  } else if (this.size === "large") {
+    sizeprice = 14
+  } 
+  return sizeprice;
 }
 // add 1 for each topping
-  Pizza.prototype.toppings = function(){
+  Pizza.prototype.toppingsPrice = function(){
   let toppingprice = 0
     if (this.topping === "mushroom") {
-      return toppingprice + 1;
+      toppingprice = 1;
     } else if (this.topping === "olives"){
-      return toppingprice + 1;
+      toppingprice = 1;
     } else if (this.topping === "special"){
-      return toppingprice + 1;
+      toppingprice = 1;
     } else {
       return toppingprice;
     }
 }
 
 //calculate total price
-  Pizza.prototype.totalPrice = function(){
-  this.price = (size + topping);
-  return this.price;
-}
+//  Pizza.prototype.totalPrice = function(){
+//  {this.price = this.size + this.topping}
+//  return this.price;
+//}
 
 //UI logic
 
@@ -44,7 +45,6 @@ let size = $("select#pizzaSize").val();
 let topping = $("select#toppings").val();
 
 let pizza = new Pizza(size, topping);
-$(".topping").text(pizza.toppings())
 $(".pizza-time").text("Your pizza is a " + size + " with " + topping + " topping and will cost $" + pizza.sizePrice());
 event.preventDefault();
 });
